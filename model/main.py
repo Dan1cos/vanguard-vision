1f380beb54c8643bec8a715711c7cbd7e7a9bb24
Artem Shakirov <art.sha03@gmail.com>
2025-11-14 15:06:07 +0100

    docs: update requirements

--- model/main.py
+++ model/main.py
@@ -4,8 +4,12 @@ from PIL import Image
 def identify(photo):
     pass
 
-# img_path = ""
-# model = ModelFactory.create("yolo", weights_path="cls_v0.0.pt")
-# image = Image.open(img_path).convert("RGB")
-#
-# print(model.predict(img_path))
+img_path = ""
+model = ModelFactory.create("yolo", weights_path="model/cls_v0.0.pt")
+image = Image.open(img_path).convert("RGB")
+result = model.predict(image)
+
+top_conf = result[0].probs.top1conf
+top_name = result[0].names[result[0].probs.top1]
+
+print(f'Result: {top_name} {float(top_conf):.2f}')
